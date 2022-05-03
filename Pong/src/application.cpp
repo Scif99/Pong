f3655eff187dfec3b9a15acac02f01a01fe3bf55
Soleh <scifonelli1@yahoo.com>
2022-05-03 13:45:36 +0200

    Added basic collision detection

--- Pong/src/application.cpp
+++ Pong/src/application.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <vector>
+
+
 
 struct Ball
 {
@@ -12,12 +15,13 @@ struct Ball
         :m_ball{ sf::CircleShape(radius) }, m_bound{sf::RectangleShape(sf::Vector2f(2*radius,2*radius))}
     {
         m_ball.setPosition(300, 300);
+        m_bound.setPosition(300, 300);
+        m_bound.setOutlineColor(sf::Color::Red);
         speed.x = (rand() % 10) -5;
         speed.y = (rand() % 10) -5;
     }
     
-    void update();
-    
+    void update(const sf::RectangleShape& paddle1, const sf::RectangleShape& paddle2, int w_size);
     sf::CircleShape m_ball;
     sf::Vector2f speed;
     sf::RectangleShape m_bound; //bounding box
@@ -25,20 +29,40 @@ struct Ball
 
 };
 
-void Ball::update()
+bool checkCollides(Ball& b, const sf::RectangleShape& paddle1, const sf::RectangleShape& paddle2, int w_size)
 {
-    auto pos = m_ball.getPosition();
+    //Left paddle
+    if (b.m_bound.getPosition().x < paddle1.getPosition().x + paddle1.getSize().x
+        && b.m_bound.getPosition().y + b.m_bound.getSize().y > paddle1.getPosition().y
+        && b.m_bound.getPosition().y < paddle1.getPosition().y + paddle1.getSize().y)
+        return true;
+
+    else if (b.m_bound.getPosition().x + b.m_bound.getSize().x > paddle2.getPosition().x
+        && b.m_bound.getPosition().y + b.m_bound.getSize().y > paddle2.getPosition().y
+        && b.m_bound.getPosition().y < paddle2.getPosition().y + paddle2.getSize().y)
+        return true;
+
+    else return false;
+
+}
+
+void Ball::update(const sf::RectangleShape& paddle1, const sf::RectangleShape& paddle2, int w_size)
+{
+    auto pos = m_bound.getPosition();
+
+    //Vertical collisions
+    if (pos.y < 0 || pos.y + m_bound.getSize().y> w_size)
+    {
+        speed.y = -speed.y;
+    }
+
+    //Horizontal collisions
+    if (checkCollides(*this, paddle1, paddle2, w_size)) speed.x = -speed.x;
 
 
+    m_bound.setPosition(pos.x + speed.x, pos.y + speed.y);
     m_ball.setPosition(pos.x + speed.x, pos.y + speed.y);
 }
-
-bool in_bounds(const sf::Vector2f& pos, const sf::CircleShape& ball)
-{
-    return true;
-}
-
-
 
 
 
@@ -49,8 +73,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode(w_size,w_size), "Pong");
 
     //Dimensions of paddles
-    float  len{ 20.f };
-    float height{50.f};
+    float  len{ 10.f };
+    float height{60.f};
     float paddle_speed = 10.f;
 
     //Left paddle
@@ -63,22 +87,25 @@ int main()
 
     //Right paddle
     sf::RectangleShape board2(sf::Vector2f(len, height));
-    board2.setPosition(w_size - 20.f, 0.f);
+    board2.setPosition(w_size - len, 0.f);
     board2.setFillColor(sf::Color::White);
     board1.setOutlineColor(sf::Color::Black);
     board1.setOutlineThickness(-1.f);
 
     //Ball
-    std::unique_ptr<Ball> pball{ std::make_unique<Ball>(10.f) };
+    srand(time(NULL));
+    std::vector<std::unique_ptr<Ball>> vballs;
+    vballs.push_back(std::make_unique<Ball>(10.f));
 
 
     sf::Clock clock;
     while (window.isOpen())
     {
+        for (const auto& pball : vballs)
+        {
+            pball->update(board1,board2, w_size);
+        }
 
-        pball->update();
-        sf::Time elapsed = clock.getElapsedTime();
-        std::cout << 1/elapsed.asSeconds() << '\n';
         window.setFramerateLimit(60);
 
         clock.restart();
@@ -126,6 +153,22 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if(event.type == sf::Event::KeyPressed)
+                if (event.key.code ==sf::Keyboard::Space)
+                {
+                    srand(time(NULL));
+                    vballs.pop_back();
+                    vballs.push_back(std::make_unique<Ball>(10.f));
+                }
+                else if (event.key.code == sf::Keyboard::E)
+                {
+                    for (const auto& pball : vballs)
+                    {
+                        pball->speed += sf::Vector2f(1.f, 1.f);
+                    }
+                }
+
         }
 
 
@@ -135,7 +178,12 @@ int main()
         window.clear();
         window.draw(board1);
         window.draw(board2);
-        window.draw(pball->m_ball);
+        for (const auto& pball : vballs)
+        {
+            window.draw(pball->m_ball);
+            window.draw(pball->m_bound);
+        
+        }
         window.display();
     }
 
